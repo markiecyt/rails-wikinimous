9ad08a9d2488fe1f49a79dbdb5c25bc04ef06302
Markie Choi <markie054@gmail.com>
2022-02-15 16:51:21 +0900

    Finished rails wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   get '/articles', to: 'articles#index'
   get '/articles/:id', to: 'articles#show', as: :article
+
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch '/articles/:id', to: 'articles#update'
+
+  delete '/articles/:id', to: 'articles#destroy'
 end
