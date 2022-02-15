d613f311b86a41ba1d59866af2206739ae5f6274
Markie Choi <markie054@gmail.com>
2022-02-15 16:05:26 +0900

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,25 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new.html.erb'
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/articles/new', to: 'articles#new', as: :new_article
+  post '/articles', to: 'articles#create', as: :articles
+
   get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show', as: :article
 end
